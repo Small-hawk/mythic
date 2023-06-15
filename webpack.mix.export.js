4ca00fa182087bcb30b42cd3511bca44f0abfd1a
Stelios Karakostas <kakaste92@gmail.com>
2023-06-16 01:13:35 +0300

    :bug: Fix bugs with rename

--- webpack.mix.export.js
+++ webpack.mix.export.js
@@ -5,10 +5,10 @@
  * uploading to a site or zipping. Edit the `files` or `folders` variables if
  * you need to change something.
  *
- * @package   newnewnewmythic
+ * @package   Mythic
  * @author    Justin Tadlock <justintadlock@gmail.com>
  * @copyright 2018 Justin Tadlock
- * @link      https://themehybrid.com/themes/newnewmythic
+ * @link      https://themehybrid.com/themes/mythic
  * @license   https://www.gnu.org/licenses/gpl-2.0.html GPL-2.0-or-later
  */
 
@@ -18,7 +18,7 @@ const rimraf  = require( 'rimraf' );
 const fs      = require( 'fs' );
 
 // Folder name to export the files to.
-let exportPath = 'newnewmythic';
+let exportPath = 'mythic';
 
 // Theme root-level files to include.
 let files = [
